c85cf4655a83397a646af9beac84abe1f4c86f9a
Tom Wu <twu@lcs.on.ca>
2024-01-18 11:42:44 -0500

    +tab view

---
+++ EnhancedToDoList/Views/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  EnhancedToDoList
+//
+//  Created by Tom Wu on 2024-01-18.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
