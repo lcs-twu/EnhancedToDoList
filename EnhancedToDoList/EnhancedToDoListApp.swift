2fd15c0f2522cfdb2135fe00d6482ba0c9925044
Tom Wu <twu@lcs.on.ca>
2024-01-18 11:34:11 -0500

    Initial Commit

---
+++ EnhancedToDoList/EnhancedToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedToDoListApp.swift
+//  EnhancedToDoList
+//
+//  Created by Tom Wu on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
