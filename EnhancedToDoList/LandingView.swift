fa1984b56d4d3678841bc44499c77f869478f8be
Tom Wu <twu@lcs.on.ca>
2024-01-18 11:39:53 -0500

    +landingview +viewsgroup +modelsgroup

---
+++ EnhancedToDoList/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  EnhancedToDoList
+//
+//  Created by Tom Wu on 2024-01-18.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
