7f5570fa8cf3668ec032d3ab839b345c8f9396af
Tom Wu <twu@lcs.on.ca>
2024-01-19 14:07:48 -0500

    Create our permanent storage (if not already there). Create this model (TodoItem) in our permanent storage (if not already there). Load the model into the model context (more on this later). Add the model context to the environment of the view (or window group) it is attached to.

--- EnhancedToDoList/EnhancedToDoListApp.swift
+++ EnhancedToDoList/EnhancedToDoListApp.swift
@@ -5,6 +5,7 @@
 //  Created by Tom Wu on 2024-01-18.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct EnhancedToDoListApp: App {
         WindowGroup {
             LandingView()
         }
+        .modelContainer(for: TodoItem.self)
     }
 }
